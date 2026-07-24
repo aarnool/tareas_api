1b481e3623b4b0d66dc95ce26fc9d65de0d89055
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-07-24 17:39:40 -0500

    feat: se enlazo el rooter de user a main.py para el entrypoint principal

--- app/src/main.py
+++ app/src/main.py
@@ -3,6 +3,11 @@ from app.src.database import engine, Base
 from app.src.domains.users import models as user_models
 from app.src.domains.tasks import models as task_models
 
+from app.src.domains.users.router import router as user_router
+
 Base.metadata.create_all(bind=engine)
 
+
+
 app = FastAPI(title="Task Management API", version="1.0.0")
+app.include_router(user_router)
