ad2e97b9a5b55ed9708ff63a05b444933e1e00a5
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-07-25 18:50:47 -0500

    feat: se creo los endpoint de login/logout

--- src/domains/users/router.py
+++ src/domains/users/router.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, Body, Depends, HTTPException, Path, Query, status
+from fastapi import APIRouter, Body, Depends, HTTPException, Path, Query, status, Response
 from fastapi.security import OAuth2PasswordRequestForm
 from typing import Annotated
 from sqlalchemy.orm import Session
 from src.domains.users.schemas import UserResponse, UserCreate
-from src.domains.users.dependencies import get_db
+from src.domains.users.dependencies import get_db, get_current_user
 from sqlalchemy import select
 from src.domains.users.models import User
 from src.core.security import get_password_hash
-from src.core.security import verify_password, DUMMY_HASH
+from src.core.security import verify_password, DUMMY_HASH, create_access_token, ACCESS_TOKEN_EXPIRE_MINUTES
 
 router = APIRouter(
     prefix="/users",
@@ -51,3 +51,69 @@ def register_user(
     db.refresh(new_user)
     return new_user
 
+@router.post(
+        "/login",
+        summary="Authenticate a user/Autenticar un usuario",
+        description="Authenticate a user with the provided credentials./Autenticar un usuario con las credenciales proporcionadas.",
+        status_code=status.HTTP_200_OK)
+
+def login_user(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Annotated[Session, Depends(get_db)],
+    response: Response):
+
+    stmt = select(User).where(User.email == form_data.username)
+    user = db.scalar(stmt)
+    if not user:
+        verify_password(form_data.password, DUMMY_HASH)  # Prevent timing attacks
+        raise HTTPException(
+            status_code=400, 
+            detail="Incrorrect email or passwoard/Correo o Contraseña incorrecta")
+    
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=400, 
+            detail="Incrorrect email or passwoard/Correo o Contraseña incorrecta")
+
+    auth_token = create_access_token(
+        data={"sub": user.username, "user_id": user.id}
+    )
+
+    response.set_cookie(
+        key="auth_token",
+        value=auth_token,
+        httponly=True,
+        secure=True,
+        samesite="lax",
+        max_age= ACCESS_TOKEN_EXPIRE_MINUTES * 60
+    )
+
+    return {
+        "message": "Login successful/Inicio de sesión exitoso"
+    }
+
+@router.get(
+        "/me",
+        summary="Get current user information/Obtener información del usuario actual",
+        description="Retrieve the information of the authenticated user./Recuperar la información del usuario autenticado.",
+        response_model=UserResponse,)
+
+def get_current_user_info(
+    current_user: Annotated[dict, Depends(get_current_user)],
+    db: Annotated[Session, Depends(get_db)]):
+
+    stmt = select(User).where(User.id == current_user["user_id"])
+    user = db.scalar(stmt)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="User not found/Usuario no encontrado"
+        )
+    
+    return user
+
+
+@router.post("/logout")
+def logout(response: Response):
+    response.delete_cookie("auth_token", path="/")
+    return {"message": "Sesión cerrada"}
