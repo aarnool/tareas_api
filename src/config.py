b0c2a5e826ac21c2238d22467c68ec471195e4aa
aarnoolxd <166175595+aarnool@users.noreply.github.com>
2026-07-25 16:53:49 -0500

    feat: se configuro la creacion de tokens-jwt

--- src/config.py
+++ src/config.py
@@ -9,6 +9,7 @@ class Settings(BaseSettings):
     DB_HOST: str
     DB_PORT: int
     DB_NAME: str
+    SECRET_KEY: SecretStr
 
     model_config = SettingsConfigDict(
         env_file=".env", 
--- src/core/security.py
+++ src/core/security.py
@@ -1,4 +1,7 @@
+from datetime import datetime, timedelta, timezone
 from  pwdlib import PasswordHash
+import jwt
+from src.config import settings
 
 password_hash = PasswordHash.recommended()
 DUMMY_HASH = password_hash.hash("dummy_password")   
@@ -8,3 +11,17 @@ def get_password_hash(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return password_hash.verify(plain_password, hashed_password)
+
+SECRET_KEY = settings.SECRET_KEY.get_secret_value()
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+def create_access_token(data: dict, expires_delta: timedelta | None = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.now(timezone.utc) + expires_delta
+    else:
+        expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
